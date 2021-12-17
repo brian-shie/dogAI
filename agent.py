19400e157aecd89f5377fc97f2621695962916a8
Brian Hashimoto Shie <brian.hshie@gmail.com>
2021-12-17 07:44:20 -0300

    Add initial model, agent and AI codes

--- agent.py
+++ agent.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 from collections import deque
 from scipy.stats import bernoulli
+from model import Linear_QNet, QTrainer
+from dogAI import DogGameAI
 
 max_memory = 100_000
 batch_size = 1000
@@ -14,15 +16,15 @@ class Agent:
 		self.n_games = 0
 		self.epsilon = 0 # aleatoriedade
 		self.gamma = 0 # taxa de desconto
-		self.memory = deque(maxlen = MAX_MEMORY) # se alcança o limite: popleft()
-		self.model = None
-		self.trainer = None
+		self.memory = deque(maxlen = max_memory) # se alcança o limite: popleft()
+		self.model = Linear_QNet(10, 256, 5)
+		self.trainer = QTrainer(self.model, lr=learning_rate, gamma=self.gamma)
 
 	def get_state(self, game):
 		state = [
-			dog.x, dog.y, dog.mana, score, # Player
-			biscoito.rng_x, biscoito.rng_y, # Biscoito
-			deathspirit1.x, deathspirit1.y, deathspirit2.x, deathspirit2.y # Mobs
+			game.player.x, game.player.y, game.player.mana, game.score, # Player
+			game.biscoito.x, game.biscoito.y, # Biscoito
+			game.monster_1.x, game.monster_1.y, game.monster_2.x, game.monster_2.y # Mobs
 		]
 		return np.array(state)
 
@@ -36,23 +38,25 @@ class Agent:
 		else:
 			mini_sample = self.memory
 
-		state, action, reward, next_state, game_over = zip(*mini_sample)
-		self.trainer.train_step(state, action, reward, next_state, game_over)
+		states, actions, rewards, next_states, game_overs = zip(*mini_sample)
+		self.trainer.train_step(states, actions, rewards, next_states, game_overs)
 
 	def train_short_memory(self, state, action, reward, next_state, game_over):
 		self.trainer.train_step(state, action, reward, next_state, game_over)
 
 	def get_action(self, state):
 		# random moves: tradeoff exploration / exploitation
-		self.epsilon = 620 - 100*np.log(self.n_games)
+		# self.epsilon = 620 - 100*np.log(self.n_games)
+		self.epsilon = 150 - self.n_games
 		final_move = [0, 0, 0, 0, 0, 0]
-		if random.randint(0, 1000) , self.epsilon:
+		if random.randint(0, 250) < self.epsilon:
 			final_move = bernoulli.rvs(size = 5, p = 0.5)
 
 		else:
-			state0 = torch.tensor(state, dtype = torch.float)
-			final_move = self.model.predict_proba(state0)
+			state0 =  torch.tensor(np.array(state), dtype=torch.float)
+			final_move = self.model(state0)
 
+		return final_move
 
 def train():
 	plot_scores = []
@@ -73,16 +77,19 @@ def train():
 		state_new = agent.get_state(game)
 
 		# train short memory
-		agent.train_short_memory(state_old, final_move, reward, state_new, game_over)
+		agent.train_short_memory(state_old, final_move, reward, state_new, game.game_over)
 
-		if game_over:
+		# remember
+		agent.remember(state_old, final_move, reward, state_new, game.game_over)
+
+		if game.game_over:
 			# train long memory / replay memory / experience replay
 			game.reset()
 			agent.n_games += 1
 			agent.train_long_memory()
 
-			if score > record:
-				record = score
+			if score > best_score:
+				best_score = score
 				# agent.model.save()
 
 			print(f"Game:{agent.n_games} Score {score} Best Score {best_score} ")
